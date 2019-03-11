452053f68b6a600e207f39d6d457e17e77c10850
Tess <tess.thornley@gmail.com>
2019-03-11 18:07:04 +0000

    print lowercase added

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,9 @@
+var getHTML = require('./http-functions');
+
+function printLowerCase (html) {
+    html.map(function(x) {
+        console.log(x.toLowerCase());
+    });
+}   
+
+getHTML({host: 'sytantris.github.io', path: '/http-examples/step6/uppercase.html'}, printLowerCase);
