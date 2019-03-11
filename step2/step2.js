582d339bec0060842e4bf01af7c1d13296aeaa78
Tess <tess.thornley@gmail.com>
2019-03-11 16:16:40 +0000

    code added

---
+++ step2/step2.js
@@ -0,0 +1,25 @@
+function getAndPrintHTML () {
+    var https = require('https'); 
+
+    var dataResults = [];
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
+  
+    https.get(requestOptions, function (response){
+        
+        response.setEncoding('utf8');
+
+        response.on('data', function(data){
+            dataResults.push(data);
+        });
+
+        response.on('end', function(){
+            console.log(dataResults);
+        });
+    }); 
+};
+
+getAndPrintHTML();
